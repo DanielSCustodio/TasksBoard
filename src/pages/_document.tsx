2b8cc00f4fe293b7fa4026c41f51fd6b8154ffa6
Daniel Custodio <play.daniel@hotmail.com>
2022-10-05 21:45:49 -0300

    feat: Created home page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,10 +8,15 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;900&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;700;900&display=swap"
             rel="stylesheet"
           />
-          <link rel="shortcut icon" href="/favicon.svg" type="image/svg" />
+          <link
+            rel="shortcut icon"
+            href="images/favicon.svg"
+            type="image/svg"
+          />
+          <title>TasksBoard</title>
         </Head>
         <body>
           <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,41 @@ import React from 'react';
 import style from '../styles/home.module.sass';
 
 export default function Home() {
-  return <div className={style.title}>index</div>;
+  return (
+    <main className={style.contentContainer}>
+      <div className={style.banner}>
+        <h1>
+          Tasks<span>Board</span>
+        </h1>
+        <p>Nunca foi tão fácil se organizar</p>
+      </div>
+
+      <section className={style.callToAction}>
+        <h2>
+          Organização é <span>tudo</span>!<br /> E vamos te ajudar com isso.
+        </h2>
+        <p>
+          Experimente <span>agora</span> mesmo!
+        </p>
+      </section>
+      <div className={style.donatersContainer}>
+        <h2>Apoiadores</h2>
+        <div className={style.donatersContent}>
+          <img src="images/daniel.png" alt="Usuário doador" />
+          <img src="images/daniel.png" alt="Usuário doador" />
+          <img src="images/daniel.png" alt="Usuário doador" />
+          <img src="images/daniel.png" alt="Usuário doador" />
+          <img src="images/daniel.png" alt="Usuário doador" />
+          <img src="images/daniel.png" alt="Usuário doador" />
+          <img src="images/daniel.png" alt="Usuário doador" />
+          <img src="images/daniel.png" alt="Usuário doador" />
+          <img src="images/daniel.png" alt="Usuário doador" />
+          <img src="images/daniel.png" alt="Usuário doador" />
+          <img src="images/daniel.png" alt="Usuário doador" />
+          <img src="images/daniel.png" alt="Usuário doador" />
+          <img src="images/daniel.png" alt="Usuário doador" />
+        </div>
+      </div>
+    </main>
+  );
 }
