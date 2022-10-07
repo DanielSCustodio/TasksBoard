cf07124b9a3d4508efba5ac312936e73f2fd91f1
Daniel Custodio <play.daniel@hotmail.com>
2022-10-07 17:08:46 -0300

    feat: Created SEO component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,46 @@
 import React from 'react';
+import SEO from '../components/SEO';
 import style from '../styles/home.module.sass';
 
 export default function Home() {
   return (
-    <main className={style.contentContainer}>
-      <div className={style.banner}>
-        <h1>
-          Tasks<span>Board</span>
-        </h1>
-        <p>Nunca foi tão fácil se organizar</p>
-      </div>
-
-      <section className={style.callToAction}>
-        <h2>
-          Organização é <span>tudo</span>!<br /> E vamos te ajudar com isso.
-        </h2>
-        <p>
-          Experimente <span>agora</span> mesmo!
-        </p>
-      </section>
-      <div className={style.donatersContainer}>
-        <h2>Apoiadores</h2>
-        <div className={style.donatersContent}>
-          <img src="images/daniel.png" alt="Usuário doador" />
-          <img src="images/daniel.png" alt="Usuário doador" />
-          <img src="images/daniel.png" alt="Usuário doador" />
-          <img src="images/daniel.png" alt="Usuário doador" />
-          <img src="images/daniel.png" alt="Usuário doador" />
-          <img src="images/daniel.png" alt="Usuário doador" />
-          <img src="images/daniel.png" alt="Usuário doador" />
-          <img src="images/daniel.png" alt="Usuário doador" />
-          <img src="images/daniel.png" alt="Usuário doador" />
-          <img src="images/daniel.png" alt="Usuário doador" />
-          <img src="images/daniel.png" alt="Usuário doador" />
-          <img src="images/daniel.png" alt="Usuário doador" />
-          <img src="images/daniel.png" alt="Usuário doador" />
+    <>
+      <SEO title="Início" />
+      <main className={style.contentContainer}>
+        <div className={style.banner}>
+          <h1>
+            Tasks<span>Board</span>
+          </h1>
+          <p>Nunca foi tão fácil se organizar</p>
         </div>
-      </div>
-    </main>
+
+        <section className={style.callToAction}>
+          <h2>
+            Organização é <span>tudo</span>!<br /> E vamos te ajudar com isso.
+          </h2>
+          <p>
+            Experimente <span>agora</span> mesmo!
+          </p>
+        </section>
+        <div className={style.donatersContainer}>
+          <h2>Apoiadores</h2>
+          <div className={style.donatersContent}>
+            <img src="images/daniel.png" alt="Usuário doador" />
+            <img src="images/daniel.png" alt="Usuário doador" />
+            <img src="images/daniel.png" alt="Usuário doador" />
+            <img src="images/daniel.png" alt="Usuário doador" />
+            <img src="images/daniel.png" alt="Usuário doador" />
+            <img src="images/daniel.png" alt="Usuário doador" />
+            <img src="images/daniel.png" alt="Usuário doador" />
+            <img src="images/daniel.png" alt="Usuário doador" />
+            <img src="images/daniel.png" alt="Usuário doador" />
+            <img src="images/daniel.png" alt="Usuário doador" />
+            <img src="images/daniel.png" alt="Usuário doador" />
+            <img src="images/daniel.png" alt="Usuário doador" />
+            <img src="images/daniel.png" alt="Usuário doador" />
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
