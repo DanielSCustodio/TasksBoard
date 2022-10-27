69447576adf02ee733a2f34594117a7bed86a33b
Daniel Custodio <play.daniel@hotmail.com>
2022-10-27 16:33:53 -0300

    feat: Add loading component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { signIn, useSession } from 'next-auth/client';
 import firebase from '../services/firebaseConnection';
 import SEO from '../components/SEO';
 import style from '../styles/home.module.sass';
+import Loading from '../components/Loading';
 
 interface HomeProps {
   data: string;
@@ -24,116 +25,128 @@ export default function Home({ data }: HomeProps) {
   const [donaters] = React.useState<Data[]>(JSON.parse(data));
   const [session] = useSession();
 
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setLoading(true);
+    }, 1000);
+  }, []);
+
   return (
     <>
       <SEO title="Início" />
-      <main>
-        <div className={style.banner}>
-          <h1>
-            Tasks<span>Board</span>
-          </h1>
-          <p>Nunca foi tão fácil se organizar</p>
-          {!session && (
-            <>
-              <h3>Cadastro rápido e fácil.</h3>
+      {!loading ? (
+        <Loading />
+      ) : (
+        <main>
+          <div className={style.banner}>
+            <h1>
+              Tasks<span>Board</span>
+            </h1>
+            <p>Nunca foi tão fácil se organizar</p>
+            {!session && (
+              <>
+                <h3>Cadastro rápido e fácil.</h3>
 
-              <button
-                type="button"
-                className={style.SigInButtonGitHub}
-                onClick={() => signIn('github')}
-              >
-                <FaGithub size={25} />
-                Entrar com GitHub
-              </button>
-              <button
-                type="button"
-                className={style.SigInButtonGoogle}
-                onClick={() => signIn('google')}
-              >
-                <ImGoogle3 size={25} />
-                Entrar com Google
-              </button>
-            </>
-          )}
-        </div>
-
-        <section className={style.callToAction}>
-          <h2>
-            Organização é <span>tudo</span>!<br /> E vamos te ajudar com isso.
-          </h2>
-          <p>
-            Experimente <span>agora</span> mesmo!
-          </p>
-        </section>
-
-        <section className={style.prices}>
-          <div>
-            <section>
-              <p>Free</p>
-              <h3>R$ 0,00</h3>
-            </section>
-            <ul>
-              <li>
-                <img src="/images/check.png" width="20px" />
-                Criar tarefa
-              </li>
-              <li>
-                <img src="/images/check.png" width="20px" />
-                Excluir tarefa
-              </li>
-            </ul>
-            <img
-              src="/images/free.png"
-              alt="icone do pacote free"
-              width="35px"
-            />
-          </div>
-          <div>
-            <section>
-              <p>Apoiador</p>
-              <h3>R$ 0,01</h3>
-            </section>
-            <ul>
-              <li>
-                <img src="/images/check.png" width="20px" />
-                Criar tarefa
-              </li>
-              <li>
-                <img src="/images/check.png" width="20px" />
-                Excluir tarefa
-              </li>
-              <li>
-                <img src="/images/check.png" width="20px" />
-                Editar tarefa
-              </li>
-              <li>
-                <img src="/images/check.png" width="20px" />
-                Foto na página inicial
-              </li>
-              <li>
-                <img src="/images/check.png" width="20px" />
-                Página de detalhe da tarefa
-              </li>
-            </ul>
-            <img
-              src="/images/premium.png"
-              alt="icone do pacote premium"
-              width="35px"
-            />
-          </div>
-        </section>
-        <div className={style.donatersContainer}>
-          {donaters.length >= 1 && <h2>Apoiadores</h2>}
-          <div className={style.donatersContent}>
-            {donaters.map(
-              (item) =>
-                item.image && (
-                  <img key={item.id} src={item.image} alt={item.name} />
-                ),
+                <button
+                  type="button"
+                  className={style.SigInButtonGitHub}
+                  onClick={() => signIn('github')}
+                >
+                  <FaGithub size={25} />
+                  Entrar com GitHub
+                </button>
+                <button
+                  type="button"
+                  className={style.SigInButtonGoogle}
+                  onClick={() => signIn('google')}
+                >
+                  <ImGoogle3 size={25} />
+                  Entrar com Google
+                </button>
+              </>
             )}
           </div>
-        </div>
-      </main>
+
+          <section className={style.callToAction}>
+            <h2>
+              Organização é <span>tudo</span>!<br /> E vamos te ajudar com isso.
+            </h2>
+            <p>
+              Experimente <span>agora</span> mesmo!
+            </p>
+          </section>
+
+          <section className={style.prices}>
+            <div>
+              <section>
+                <p>Free</p>
+                <h3>R$ 0,00</h3>
+              </section>
+              <ul>
+                <li>
+                  <img src="/images/check.png" width="20px" />
+                  Criar tarefa
+                </li>
+                <li>
+                  <img src="/images/check.png" width="20px" />
+                  Excluir tarefa
+                </li>
+              </ul>
+              <img
+                src="/images/free.png"
+                alt="icone do pacote free"
+                width="35px"
+              />
+            </div>
+            <div>
+              <section>
+                <p>Apoiador</p>
+                <h3>R$ 0,01</h3>
+              </section>
+              <ul>
+                <li>
+                  <img src="/images/check.png" width="20px" />
+                  Criar tarefa
+                </li>
+                <li>
+                  <img src="/images/check.png" width="20px" />
+                  Excluir tarefa
+                </li>
+                <li>
+                  <img src="/images/check.png" width="20px" />
+                  Editar tarefa
+                </li>
+                <li>
+                  <img src="/images/check.png" width="20px" />
+                  Foto na página inicial
+                </li>
+                <li>
+                  <img src="/images/check.png" width="20px" />
+                  Página de detalhe da tarefa
+                </li>
+              </ul>
+              <img
+                src="/images/premium.png"
+                alt="icone do pacote premium"
+                width="35px"
+              />
+            </div>
+          </section>
+          <div className={style.donatersContainer}>
+            {donaters.length >= 1 && <h2>Apoiadores</h2>}
+            <div className={style.donatersContent}>
+              {donaters.map(
+                (item) =>
+                  item.image && (
+                    <img key={item.id} src={item.image} alt={item.name} />
+                  ),
+              )}
+            </div>
+          </div>
+        </main>
+      )}
     </>
   );
 }
